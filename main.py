d8dfa5f90e2370dd9f6defa15dc582caa946d1e4
yinelos <77772714+yinelos@users.noreply.github.com>
2022-01-22 13:59:00 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,20 @@
+# Function that return the direction you will face after the turn.
+def direction(facing:str, turn:int) -> str:
+    # Define list of directions
+    directions = ['N', 'NE', 'E', 'SE', 'S', 'SW', 'W', 'NW']
+    
+    # Get index of direction
+    try:
+        start_position = directions.index(facing)
+    except ValueError:
+        return "Error: 'facing' must be one of %s directions" % (str(directions))
+    
+    # Get required steps
+    try:
+        steps = turn//45
+    except TypeError:
+         return "Error: 'turn' must be <int>"
+    
+    # Calculate end position direction index
+    end_position = (start_position+steps)%8
+    return directions[end_position]
